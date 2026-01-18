141df61ca7dca5fd5872e39b4ca3e23cd9601fcf
Misha <luffimugivara684@gmail.com>
2026-01-18 14:06:34 +0300

    Empty main branch

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/nightmaker00/go-tasks-api
-
-go 1.22.0
-
-require github.com/lib/pq v1.10.9 // indirect
--- migrations/000001_task.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    description TEXT,
-    status TEXT NOT NULL DEFAULT 'new',
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
